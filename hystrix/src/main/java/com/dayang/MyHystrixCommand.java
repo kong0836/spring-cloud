34002464415a44031d1dd3080eeb0b793ed36324
konglinghui <konglinghui@dayang.com.cn>
2021-05-07 14:28:46 +0800

    hytrix简单使用

---
+++ hystrix/src/main/java/com/dayang/MyHystrixCommand.java
@@ -0,0 +1,37 @@
+package com.dayang;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @author konglinghui
+ * @description
+ * @date 2021/5/7 14:24
+ */
+public class MyHystrixCommand extends HystrixCommand<String> {
+
+    private final String name;
+
+    public MyHystrixCommand(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("MyGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() {
+        return this.name + ":" + Thread.currentThread().getName();
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 同步调用方式
+        // String result = new MyHystrixCommand("zhangsan").execute();
+        // System.out.println(result);
+
+        // 异步调用方式
+        Future<String> future = new MyHystrixCommand("zhangsan").queue();
+        System.out.println(future.get());
+    }
+}
