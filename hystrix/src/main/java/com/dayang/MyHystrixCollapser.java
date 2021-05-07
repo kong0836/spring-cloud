da2e354e83432087f9ed69e755eb4d624d413048
konglinghui <konglinghui@dayang.com.cn>
2021-05-07 15:47:25 +0800

    Hystrix资源隔离策略、Hystrix缓存和合并请求

---
+++ hystrix/src/main/java/com/dayang/MyHystrixCollapser.java
@@ -0,0 +1,62 @@
+package com.dayang;
+
+import com.netflix.hystrix.HystrixCollapser;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author konglinghui
+ * @description 合并请求
+ * @date 2021/5/7 15:43
+ */
+public class MyHystrixCollapser extends HystrixCollapser<List<String>, String, String> {
+    private final String name;
+
+    public MyHystrixCollapser(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getRequestArgument() {
+        return name;
+    }
+
+    @Override
+    protected HystrixCommand<List<String>> createCommand(final Collection<CollapsedRequest<String, String>> requests) {
+        return new BatchCommand(requests);
+    }
+
+    @Override
+    protected void mapResponseToRequests(List<String> batchResponse,
+                                         Collection<CollapsedRequest<String, String>> requests) {
+        int count = 0;
+        for (CollapsedRequest<String, String> request : requests) {
+            request.setResponse(batchResponse.get(count++));
+        }
+    }
+
+    private static final class BatchCommand extends HystrixCommand<List<String>> {
+        private final Collection<CollapsedRequest<String, String>> requests;
+
+        private BatchCommand(Collection<CollapsedRequest<String, String>> requests) {
+            super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"))
+                    .andCommandKey(HystrixCommandKey.Factory.asKey("GetValueForKey")));
+            this.requests = requests;
+        }
+
+        @Override
+        protected List<String> run() {
+            System.out.println(" 真正执行请求......");
+            ArrayList<String> response = new ArrayList<>();
+            for (CollapsedRequest<String, String> request : requests) {
+                response.add(" 返回结果 : " + request.getArgument());
+            }
+            return response;
+        }
+    }
+}
--- hystrix/src/main/java/com/dayang/MyHystrixCommand.java
+++ hystrix/src/main/java/com/dayang/MyHystrixCommand.java
@@ -2,6 +2,9 @@ package com.dayang;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -16,28 +19,50 @@ public class MyHystrixCommand extends HystrixCommand<String> {
     private final String name;
 
     public MyHystrixCommand(String name) {
-        super(HystrixCommandGroupKey.Factory.asKey("MyGroup"));
+        // super(HystrixCommandGroupKey.Factory.asKey("MyGroup"));
+
+        // 信号量策略配置
+        // super(HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("MyGroup"))
+        //         .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+        //                 .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.SEMAPHORE
+        //                 )));
+
+        // 线程隔离策略配置
+        super(HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("MyGroup"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD))
+                .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter().withCoreSize(10)
+                        .withMaxQueueSize(100).withMaximumSize(100)));
+
         this.name = name;
     }
 
-    // @Override
-    // protected String run() {
-    //     return this.name + ":" + Thread.currentThread().getName();
-    // }
-
-
     @Override
     protected String run() {
-        try {
-            Thread.sleep(1000 * 10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // 模拟超时
+        // try {
+        //     Thread.sleep(1000 * 10);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        System.err.println("get data");
         return this.name + ":" + Thread.currentThread().getName();
     }
+
     @Override
     protected String getFallback() {
         return "失败了 ";
+    }
+
+    /**
+     * 结果缓存
+     *
+     * @return
+     */
+    @Override
+    protected String getCacheKey() {
+        return String.valueOf(this.name);
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -46,7 +71,30 @@ public class MyHystrixCommand extends HystrixCommand<String> {
         // System.out.println(result);
 
         // 异步调用方式
-        Future<String> future = new MyHystrixCommand("zhangsan").queue();
-        System.out.println(future.get());
+        // Future<String> future = new MyHystrixCommand("zhangsan").queue();
+        // System.out.println(future.get());
+
+        // 验证可使用缓存
+        // HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        // String result = new MyHystrixCommand("zhangsan").execute();
+        // System.out.println(result);
+        // Future<String> future = new MyHystrixCommand("zhangsan").queue();
+        // System.out.println(future.get());
+        // context.shutdown();
+
+        // 验证缓存清除
+        // HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        // String result = new ClearCacheHystrixCommand("zhangsan").execute();
+        // System.out.println(result);
+        // ClearCacheHystrixCommand.flushCache("zhangsan");
+        // Future<String> future = new ClearCacheHystrixCommand("zhangsan").queue();
+        // System.out.println(future.get());
+
+        // 合并请求
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        Future<String> f1 = new MyHystrixCollapser("zhangsan").queue();
+        Future<String> f2 = new MyHystrixCollapser("zhangsan333").queue();
+        System.out.println(f1.get() + "=" + f2.get());
+        context.shutdown();
     }
 }
